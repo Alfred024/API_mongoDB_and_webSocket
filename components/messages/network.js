30b04e0270b886aed3a62b24d175c271c4aa2d8e
Alfredo-JT <21030761itcelaya.edu.mx>
2023-09-03 10:31:01 -0600

    Created controller for adding logif to our express app

--- components/messages/network.js
+++ components/messages/network.js
@@ -1,4 +1,6 @@
 const response = require('../../network/response');
+const controller = require('./controller');
+
 const express = require('express');
 const router = express.Router();
 
@@ -8,6 +10,18 @@ router.get('/', (req, res) =>{
     } catch (error) {
         response.error( req, res, 'Error interno para obtner los mensajes', error, 500 );
     }
+});
+
+
+router.post('/', (req, res) =>{
+    body = req.body;
+    controller.addMessage(body.user, body.message)
+        .then(() =>{
+            response.succes(req, res, 'Message posted succesfully', 200);  
+        })
+        .catch(err =>{
+            response.error( req, res, err, 'Error en el controlador', 500 );
+        });
 });
 
 router.delete('/', (req, res) =>{
